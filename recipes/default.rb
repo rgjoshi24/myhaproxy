d6cecd2a52a778078e5f291822e86d4fdf852f74
Ram <rgjoshi24@gmail.com>
2018-03-29 17:45:47 -0400

    MyHAProxy which uses supermarket to create a load balancer and point to iis_web

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,12 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+node.default['haproxy']['members'] = [
+  {
+    "hostname" => "ec2-52-206-216-151.compute-1.amazonaws.com",
+    "ipaddress" => "52.206.216.151",
+    "port" => 80,
+    "ssl_port" => 80
+  }
+]
+include_recipe 'haproxy::manual'
