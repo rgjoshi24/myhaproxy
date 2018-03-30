654fbeb00a4b4e4c14700ae68decd83ce61da664
Ram <rgjoshi24@gmail.com>
2018-03-30 12:55:45 -0400

    updated the code to be dynamic using search

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,18 +3,21 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
-node.default['haproxy']['members'] = [
+all_web_nodes = search('node', 'role:web_server')
+
+members = []
+
+all_web_nodes.each do |web_node|
+member =
   {
-    "hostname" => "ec2-52-206-216-151.compute-1.amazonaws.com",
-    "ipaddress" => "52.206.216.151",
-    "port" => 80,
-    "ssl_port" => 80
-  },
-  {
-    "hostname" => "ec2-54-89-88-32.compute-1.amazonaws.com",
-    "ipaddress" => "54.89.88.32",
-    "port" => 80,
-    "ssl_port" => 80
+    'hostname' => web_node['cloud']['public_hostname'],
+    'ipaddress' => web_node['cloud']['public_ipv4'],
+    'port' => 80,
+    'ssl_port' => 80
   }
-]
+  members.push(member)
+end
+
+node.default['haproxy']['members'] = members
+
 include_recipe 'haproxy::manual'
